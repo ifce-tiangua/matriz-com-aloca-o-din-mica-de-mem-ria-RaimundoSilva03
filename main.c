576233eca2f1e5892cd477f09f83bd392533fd10
RaimundoSilva03 <126274796+RaimundoSilva03@users.noreply.github.com>
2024-11-14 12:08:41 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,47 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(){
-
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
+int main()
+{
+    int **mat, lin, col;
+    int i, j;
+    //printf("numero de linhas: \n");
+    scanf("%d", &lin);
+    //printf("numero de colunas: \n");
+    scanf("%d", &col);
+    
+    if(lin <= 0 || col <= 0) {
+        printf("[matriz vazia]");
+        return 0;
+    }   
+    
+     //Alocar a matriz
+     mat = (int**)malloc(lin*sizeof(int*));
+       
+     for(int i = 0; i < lin; i++) {
+         mat[i] = (int*)malloc(col*sizeof(int));
+     }
+   //Leitura dos elementos
+   //printf("Elementos da matriz\n");
+   for(i = 0; i < lin; i++) {
+       for(j = 0; j < col; j++){
+           scanf("%d", &mat[i][j]);
+       }
+   }
+   //Exibição da matriz
+   //printf("Matriz\n");
+   for(i = 0; i < lin; i++) {
+       
+       for(j = 0; j < col; j++) {
+           printf("%d ", mat[i][j]);
+      
+       }
+       printf("\n");
+   } 
+   //Desaloca a memória
+   for(i = 0; i < lin; i++) {
+    free(mat[i]); 
+   }
+   free(mat);
     return 0;
 }
